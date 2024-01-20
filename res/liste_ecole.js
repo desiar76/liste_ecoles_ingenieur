9a71e48f793e5a0bd39457c5e0c0784487f9b958
desiar76 <47336364+desiar76@users.noreply.github.com>
2024-01-20 20:10:38 +0100

    t

--- res/liste_ecole.js
+++ res/liste_ecole.js
@@ -3,12 +3,21 @@ const datas=Array(
         "name":"fecole1",
         "type":"dossier",
         "lien":"insert link",
-        "date":"date"
+        "date":new Date('02-20, 2024')
     },
     {
         "name":"ecole2",
         "type":"concours",
         "lien":"insert link",
-        "date":"date"
+        "date":new Date('02-21, 2024 GMT')
     }
 );
+/**Créer une nouvelle donnée :
+,
+    {
+        "name":"ecole2",
+        "type":"concours/dossier",
+        "lien":"insert link",
+        "date":Date.parse('dd-mm-2024 00:00:00 GMT')
+    }
+ */
